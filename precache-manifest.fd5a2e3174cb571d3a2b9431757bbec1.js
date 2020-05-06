c7f47476d7e3966a013f9a400b39db939f62c853
gabrielgarcia7 <gabrielgarcia7@usp.br>
2020-05-06 14:20:48 -0300

    Updates

---
+++ precache-manifest.fd5a2e3174cb571d3a2b9431757bbec1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "727273a0ae9cafe3a8bade54aac43254",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "bd100388b21213c3f938",
+    "url": "/react-todo-app/static/css/main.975636c5.chunk.css"
+  },
+  {
+    "revision": "bc79de04aef112c1e768",
+    "url": "/react-todo-app/static/js/2.e14ba549.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-app/static/js/2.e14ba549.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd100388b21213c3f938",
+    "url": "/react-todo-app/static/js/main.18d2f532.chunk.js"
+  },
+  {
+    "revision": "aeef5eaa9a208e21e8ee",
+    "url": "/react-todo-app/static/js/runtime-main.6cd1dbba.js"
+  }
+]);
