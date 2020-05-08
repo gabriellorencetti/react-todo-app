3e50b27a207da27249293202f82873ebd11832a5
gabrielgarcia7 <gabrielgarcia7@usp.br>
2020-05-08 10:16:10 -0300

    new icon

--- src/App.js
+++ src/App.js
@@ -18,7 +18,10 @@ class App extends React.Component {
     this.setState((prevState) => {
         const updatedTodos = prevState.todos.map(todo => {
           if(todo.id === id){
-            todo.completed = !todo.completed
+            return {
+              ...todo,
+              completed: !todo.completed
+            }
           }
           return todo
         })
@@ -34,6 +37,7 @@ class App extends React.Component {
 
     return (
       <div className="todo-list">
+        <header><h1>To Do List</h1></header>
         {todoItems}
       </div>
     )
